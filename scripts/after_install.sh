26e8f31261febebec2ea9343939546f0c9c220ce
Nikhil2609 <75295745+Nikhil2609@users.noreply.github.com>
2025-07-08 18:50:54 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,2 @@
 echo "Cleaning old app folder"
-
-cd /home/ec2-user/Node-EC2-codeDeploy
-
-sudo chown -R ec2-user:ec2-user /home/ec2-user/Node-EC2-codeDeploy
-
-echo "Install Stage Start"
 npm install
-
-echo "Build Stage Start"
-npm run build
