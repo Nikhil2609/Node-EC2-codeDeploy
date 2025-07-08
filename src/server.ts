e61b14c5ae187c4232e0cc95310e1a368d1468c1
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-07-08 15:13:57 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const a = 10;
+
+app.get("/", (req, res) => {
+    console.log("API request", req, res);
+    res.send("Hello World from Elastic BeanStalk 123");
+})
+
+app.listen(PORT, () => { console.log("Server Running on Port =>",PORT) })
