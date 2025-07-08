5b512de1e7b00e2b7f1a2a3178cd87c746cb0f8b
Nikhil2609 <75295745+Nikhil2609@users.noreply.github.com>
2025-07-08 18:45:28 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,4 @@
-echo "Cleaning old app folder"
-rm -rf /home/ec2-user/Node-EC2-codeDeploy
+echo "Cleaning old app folder"rm -rf /home/ec2-user/Node-EC2-codeDeploy
 
 cd /home/ec2-user/Node-EC2-codeDeploy
 
