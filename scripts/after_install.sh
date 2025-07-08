18bc2124ed01b71e49bf119f94bfa0e3d549f809
Nikhil Panchal <nikhil.panchal@tatvasoft.com>
2025-07-08 18:39:18 +0530

    changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,8 @@ rm -rf /home/ec2-user/Node-EC2-codeDeploy
 
 cd /home/ec2-user/Node-EC2-codeDeploy
 
+sudo chown -R ec2-user:ec2-user /home/ec2-user/Node-EC2-codeDeploy
+
 echo "Install Stage Start"
 npm install
 
